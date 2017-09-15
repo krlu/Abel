813c7b1f599d51c52b4710983fd4a8f8d5eef55b
Kenneth Lu <krlu91@gmail.com>
2017-09-14 22:45:58 -0400

    adding more unit tests

--- src/test/scala/EuclideanAlgorithmTest.scala
+++ src/test/scala/EuclideanAlgorithmTest.scala
@@ -6,5 +6,9 @@ class EuclideanAlgorithmTest extends FlatSpec with Matchers {
     val (gcd, _) = GCD(614,513)
     assert(gcd == 1)
     assert(ExtendedEuclideanAlgorithm(614, 513) == (-193, 231))
+    assert(ExtendedEuclideanAlgorithm(5, 7) == (3,-2))
+    assert(ExtendedEuclideanAlgorithm(5, 15) == (1,0))
+    assert(ExtendedEuclideanAlgorithm(10, 15) == (-1,1))
+    assert(ExtendedEuclideanAlgorithm(9, 16) == (-7,4))
   }
 }
