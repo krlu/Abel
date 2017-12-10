766a06d561b35b68b42d145c032a284e5d585883
Kenneth Lu <krlu91@gmail.com>
2017-12-09 20:27:16 -0500

    minor tweaks to experiment

--- src/main/scala/org/bu/metcs789/FinalProjectExperiment.scala
+++ src/main/scala/org/bu/metcs789/FinalProjectExperiment.scala
@@ -27,22 +27,26 @@ object FinalProjectExperiment{
 
     println("Sending Message as Alice")
     ElGamalAlice(857, 125, 100)
-    println
-    println(s"Sending message as Bob: ")
-    ElGamalBob(941, 141, 142, 729)
     println()
+
+    println(s"Sending message as Bob: ")
+    ElGamalBob(941, 141, 788, 142, 729)
+    println()
+
     println("Breaking El Gamal as Eve")
     ElGamalEve(1559, 569, 1372, Alice)
   }
 
-  private def RSAAlice(n: Int, g: Int, m: Int): Unit ={
+  private def RSAAlice(n: Int, g: Int, m: Int): Unit = {
     println(s"Alice sends m^g = ${FastExpWithMod(n)(m,g)}")
   }
+
   private def RSABob(p: Int, q: Int, g: Int): Unit ={
     require(IsPrime(p), IsPrime(q))
     val n = p*q
     println(s"Bob sends n = $n and g = $g")
   }
+
   private def RSADecrypt(n: Int, g: Int, y: Int, phi: Int, person: Person): Unit ={
     val  g_inv= ModInverse(g, phi)
     val name = person match {
@@ -52,6 +56,7 @@ object FinalProjectExperiment{
     }
     println(s"$name Decrypts by getting y^(-g) = ${FastExpWithMod(n)(y, g_inv)}")
   }
+
   private def RSADecryptEve(n: Int, g: Int, y: Int): Unit ={
     val p = PollardRho(n).get.toInt
     val q = n/p
@@ -63,6 +68,7 @@ object FinalProjectExperiment{
     println(s"Let p = $p, g = $g, a = $a")
     println(s"Alice sends  p = $p, g = $g, g^a = ${FastExpWithMod(p)(g,a)}")
   }
+
   private def ElGamalEve(p: Int, g: Int, g_power: Int, sender: Person): Unit ={
     val varName = sender match {
       case Alice => "a"
@@ -73,16 +79,16 @@ object FinalProjectExperiment{
     println(s"$varName = Log_${g}_$g_power = ${DiscreteLog(p)(g, g_power)}")
     println(s"Verifying that $g^${log.get} = ${FastExpWithMod(p)(g, log.get)}")
   }
-  private def ElGamalBob(p: Int, g: Int, b : Int, m : Int){
-    val g_ab = FastExpWithMod(p)(788,142)
+
+  private def ElGamalBob(p: Int, g: Int, g_a: Int, b : Int, m : Int){
+    val g_ab = FastExpWithMod(p)(g_a, b)
     println(s"Let p = $p , g = $g,  b = $b, m = $m ")
-    println(s"141 ^ 142 mod 941 = ${FastExpWithMod(p)(g,b)}")
-    println(s"788 ^ 142 mod 941 = $g_ab")
-    println(s"m*(g^(ab)) = ${(m * g_ab) % 941}")
+    println(s"g^b = ${FastExpWithMod(p)(g,b)}")
+    println(s"m*(g^(ab)) = ${(m * g_ab) % p}")
   }
 }
 
-trait Person
-case object Alice extends Person
-case object Bob extends Person
-case object Eve extends Person
+private trait Person
+private case object Alice extends Person
+private case object Bob extends Person
+private case object Eve extends Person
