e3a6aafd647f61968e537ea44cd0286fde789309
Kenneth Lu <krlu91@gmail.com>
2018-06-03 21:20:14 -0400

    renamed field to ring where appropriate

--- src/main/scala/org/bu/metcs789/algebraicStructures/polynomials/Poly.scala
+++ src/main/scala/org/bu/metcs789/algebraicStructures/polynomials/Poly.scala
@@ -4,33 +4,33 @@ import org.bu.metcs789.algebraicStructures.fields.Real
 import org.bu.metcs789.algebraicStructures.rings.Ring
 
 /**
-  * Generalization of a Polynomial
+  * Generalization of a finite Polynomial
   * @param coeffs - coefficients for polynomial
-  * @param field - Algebraic Field that governs set T of values
-  * @tparam T - Set of values coefficients can take
-  * @tparam U - Type bound on Algebraic Field
+  * @param ring - Algebraic Ring that governs set T of values
+  * @tparam T - Type bound must support Ring structure
+  * @tparam U - Type bound must extend Ring[T]
   */
-class Poly[T, U <: Ring[T]](coeffs: T*)(implicit field: U) extends (T => T){
+class Poly[T, U <: Ring[T]](coeffs: T*)(implicit ring: U) extends (T => T){
 
-  lazy val coefficients: Seq[T] = if(coeffs.isEmpty || coeffs.forall(field.eq(_, field.zero))) Seq(field.zero) else coeffs.reverse.dropWhile(field.eq(_, field.zero)).reverse
+  lazy val coefficients: Seq[T] = if(coeffs.isEmpty || coeffs.forall(ring.eq(_, ring.zero))) Seq(ring.zero) else coeffs.reverse.dropWhile(ring.eq(_, ring.zero)).reverse
   lazy val degree: Int = Math.max(0, coefficients.size - 1)
   val leadingCoeff: T = coefficients.head
 
-  def + (other: Poly[T, U]): Poly[T, U] = Poly[T, U](this.coefficients.zipAll(other.coefficients, field.zero, field.zero).map{case(a,b) => field.add(a,b)}:_*)(field)
-  def - (other: Poly[T, U]): Poly[T, U] = Poly[T, U](this.coefficients.zipAll(other.coefficients, field.zero, field.zero).map{case(a,b) => field.sub(a,b)}:_*)(field)
-  def pow (exp: Int): Poly[T, U] = if(exp == 0) Poly[T, U](field.one)(field) else this * (this pow (exp-1))
+  def + (other: Poly[T, U]): Poly[T, U] = Poly[T, U](this.coefficients.zipAll(other.coefficients, ring.zero, ring.zero).map{case(a,b) => ring.add(a,b)}:_*)(ring)
+  def - (other: Poly[T, U]): Poly[T, U] = Poly[T, U](this.coefficients.zipAll(other.coefficients, ring.zero, ring.zero).map{case(a,b) => ring.sub(a,b)}:_*)(ring)
+  def pow (exp: Int): Poly[T, U] = if(exp == 0) Poly[T, U](ring.one)(ring) else this * (this pow (exp-1))
   def * (other: Poly[T, U]): Poly[T, U] = {
-    if(this.coefficients.isEmpty || other.coefficients.isEmpty) return Poly[T, U](field.zero)(field)
+    if(this.coefficients.isEmpty || other.coefficients.isEmpty) return Poly[T, U](ring.zero)(ring)
     coefficients.indices.map{ i =>
-      val newCoeffs = (0 until i).map(_ => field.zero) ++ other.coefficients.map(c => field.mult(c,coefficients(i)))
-      Poly[T, U](newCoeffs:_*)(field)
+      val newCoeffs = (0 until i).map(_ => ring.zero) ++ other.coefficients.map(c => ring.mult(c,coefficients(i)))
+      Poly[T, U](newCoeffs:_*)(ring)
     }.reduce((p1, p2) => p1 + p2)
   }
 
   def == (other: Poly[T, U]): Boolean = this.equals(other)
   def != (other: Poly[T, U]): Boolean = !this.equals(other)
 
-  override def apply(v1: T): T = coefficients.indices.map{ i => field.mult(coefficients(i),field.pow(v1, i))}.reduce((a, b) => field.add(a, b))
+  override def apply(v1: T): T = coefficients.indices.map{ i => ring.mult(coefficients(i),ring.pow(v1, i))}.reduce((a, b) => ring.add(a, b))
   override def equals(obj: scala.Any): Boolean = obj match {
     case other: Poly[T,U] =>
       if(this.coefficients.size != other.coefficients.size) false
@@ -43,7 +43,7 @@ class Poly[T, U <: Ring[T]](coeffs: T*)(implicit field: U) extends (T => T){
       case poly: Poly[T, U] =>
         poly.coefficients.indices.map { i =>
           val c = poly.coefficients(i)
-          Poly[T,U](c)(field) * (this pow i)
+          Poly[T,U](c)(ring) * (this pow i)
         }.reduce((p1, p2) => p1 + p2)
       case _ => throw new IllegalArgumentException("input is not a polynomial!!")
     }
