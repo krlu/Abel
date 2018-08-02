cf150449f4af43b84e8c65773afa3e53945c7c3d
Kenny Lu <krlu91@gmail.com>
2018-08-02 16:42:42 -0400

    moving experiment code to TestBench.scala

--- src/main/scala/org/bu/metcs789/algebraicStructures/rings/polynomials/RealPolynomial.scala
+++ src/main/scala/org/bu/metcs789/algebraicStructures/rings/polynomials/RealPolynomial.scala
@@ -64,24 +64,3 @@ object RealPolynomial{
   def zero = new RealPolynomial(0.0)
   def one = new RealPolynomial(1.0)
 }
-
-object foo{
-  def main(args: Array[String]): Unit = {
-//    val p1 = RealPolynomial(-16, -24, -4, 10, 6, 1)
-//    val p2 = p1.derivative
-//    println(p1)
-//    println(p2)
-//    val (q,r) = p1/p2
-//    println(q * p2 + r)
-//    println(PolyUtil.GCD(p1, p2))
-    val p1 = RealPolynomial(-1,0,1)
-    val p2 = RealPolynomial(2,2)
-    val (a, b) = p2.reduceCoeffs
-    println(a,b)
-    println(p1)
-    println(p2/RealPolynomial(2))
-    println(p1 % p2)
-//    println(PolyUtil.GCD(p1, a))
-  }
-
-}
